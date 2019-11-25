d1a1a88b7a82a03f65a55a444b4701f22aa717c2
Flo <flo@MacBook-de-Anne.local>
2019-11-25 16:59:52 +0100

    un carré de texte, un bouton quitter, des choix qui marchent pas

--- kittenz.py
+++ kittenz.py
@@ -1,17 +1,25 @@
-# On importe Tkinter
+#!usr/bin/python
 from tkinter import *
 
 # On crée une fenêtre, racine de notre interface
-fenetre = Tk()
-
-#Miaou !
-champ_label = Label(fenetre, text="Miaou !")
-champ_label.pack()
+fenetre = Tk("Miaou")
 
 #zone de texte
-var_texte = StringVar()
-ligne_texte = Entry(fenetre, textvariable=var_texte, width=200)
-ligne_texte.pack()
+text = Text(fenetre)
+text.pack()
+
+#couleur du texte
+var_choix = StringVar()
+
+choix_rouge = Radiobutton(fenetre, text="Rouge", variable=var_choix, value="rouge")
+choix_vert = Radiobutton(fenetre, text="Vert", variable=var_choix, value="vert")
+choix_bleu = Radiobutton(fenetre, text="Bleu", variable=var_choix, value="bleu")
+
+choix_rouge.pack()
+choix_vert.pack()
+choix_bleu.pack()
+
+var_choix.get()
 
 #le bouton quitter
 quit = Button(fenetre, text="abandonner les bébés chats sur une aire d'autoroute", command=fenetre.quit)
